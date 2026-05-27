7adb97aa1ab005754dcecff7f63575cab2e2558b
CMMarvin <58445679+CMMarvin@users.noreply.github.com>
2026-05-27 09:35:12 -0600

    script.js

--- script.js
+++ script.js
@@ -13,3 +13,53 @@ function filtrarTrabajos() {
     }
   });
 }
+
+let slideActual = 0;
+
+const slides = document.querySelectorAll(".slide");
+const dotsContainer = document.getElementById("dots");
+
+slides.forEach((_, index) => {
+  const dot = document.createElement("span");
+  dot.classList.add("dot");
+
+  if (index === 0) {
+    dot.classList.add("active");
+  }
+
+  dot.addEventListener("click", () => {
+    mostrarSlide(index);
+  });
+
+  dotsContainer.appendChild(dot);
+});
+
+const dots = document.querySelectorAll(".dot");
+
+function mostrarSlide(index) {
+  slides[slideActual].classList.remove("active");
+  dots[slideActual].classList.remove("active");
+
+  slideActual = index;
+
+  slides[slideActual].classList.add("active");
+  dots[slideActual].classList.add("active");
+}
+
+function moverSlide(direccion) {
+  let nuevoIndex = slideActual + direccion;
+
+  if (nuevoIndex < 0) {
+    nuevoIndex = slides.length - 1;
+  }
+
+  if (nuevoIndex >= slides.length) {
+    nuevoIndex = 0;
+  }
+
+  mostrarSlide(nuevoIndex);
+}
+
+setInterval(() => {
+  moverSlide(1);
+}, 5000);
